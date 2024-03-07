72f28f1558244aa762032fa7d11bb3fa9483035a
HANNANI Mohamed <49957450+mhannani@users.noreply.github.com>
2024-03-07 20:24:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+h#!/bin/bash
 urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 
 echo -e "\nAfter registering at https://sgnify.is.tue.mpg.de/, provide your credentials:"
@@ -12,6 +12,7 @@ password=12az45qs78wx
 # rm data.zip
 
 
+echo "creating env"
 GIT_LFS_SKIP_SMUDGE=1 git submodule update --init --recursive
 conda env create -f environment.yml 
 eval "$(conda shell.bash hook)"
